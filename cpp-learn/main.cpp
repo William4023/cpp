aa03dfdfff768c15da9c710e7c7276fa0e9d5519
wy <wy@mbp1.attlocal.net>
2021-08-13 19:16:19 -0700

    usaco 2021 Jan Broze 2

--- cpp-learn/main.cpp
+++ cpp-learn/main.cpp
@@ -8,18 +8,19 @@ int main(){
     cin >> n;
     int even = 0;
     int odd = 0;
+    int arr[n];
     for(int i = 0; i<n ;i++){
         cin >> arr[i];
         if(arr[i]%2 == 0){
-            even++
+            even++;
         }
         else if(arr[i]%2 != 0){
-            odd++
+            odd++;
         }
         else{
             return 0;
         }
     }
     cout << even + odd <<endl;
-
+        
 }
